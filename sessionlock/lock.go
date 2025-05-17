d555ab59cb245d9999de94e4a041de637b698951
shelepuginivan <shelepuginivanm@gmail.com>
2025-05-17 12:20:37 +0300

    feat: gtk bindings utility functions

--- sessionlock/lock.go
+++ sessionlock/lock.go
@@ -5,6 +5,29 @@ package sessionlock
 // #include <gtk-session-lock.h>
 import "C"
 
+import (
+	"unsafe"
+
+	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/gtk"
+)
+
+func gobool(b C.gboolean) bool {
+	return b != C.FALSE
+}
+
+func nativeWindow(window *gtk.Window) *C.GtkWindow {
+	w := window.Native()
+	wp := (*C.GtkWindow)(unsafe.Pointer(w))
+	return wp
+}
+
+func nativeMonitor(monitor *gdk.Monitor) *C.GdkMonitor {
+	m := monitor.Native()
+	mp := (*C.GdkMonitor)(unsafe.Pointer(m))
+	return mp
+}
+
 func GetMajorVersion() uint {
 	v := C.gtk_session_lock_get_major_version()
 	return uint(v)
